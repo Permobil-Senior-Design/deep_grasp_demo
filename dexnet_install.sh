213c18ba673e65b78f72bd04d33367c8dee8edbb
jeoseo <jeongwoo.seo@vanderbilt.edu>
2021-11-03 11:58:45 -0500

    changed dexnet_install

--- dexnet_install.sh
+++ dexnet_install.sh
@@ -23,34 +23,4 @@ sudo apt install cmake libvtk6-dev python-vtk6 python-sip python-qt4 libosmesa6-
 # install pip deps
 python3 -m pip install -r dexnet_requirements.txt
 
-# install deps from source
-mkdir dexnet_deps
-cd dexnet_deps
 
-# install autolab modules
-git clone https://github.com/BerkeleyAutomation/autolab_core.git
-git clone https://github.com/BerkeleyAutomation/perception.git
-git clone https://github.com/BerkeleyAutomation/gqcnn.git
-git clone https://github.com/BerkeleyAutomation/visualization.git
-
-
-# install all Berkeley AUTOLAB modules
-# autolab_core
-cd autolab_core
-sudo python3 setup.py develop
-cd ..
-
-# perception
-cd perception
-sudo python3 setup.py develop
-cd ..
-
-# gqcnn
-cd gqcnn
-sudo python3 setup.py develop
-cd ..
-
-# visualization
-cd visualization
-sudo python3 setup.py develop
-cd ..
